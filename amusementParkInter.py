126d4d8369de70af29ff88837a8570d59799ebfc
Anna <asmarek@coe.edu>
2017-11-25 10:42:08 -0600

    Boxes for our parameters!

--- amusementParkInter.py
+++ amusementParkInter.py
@@ -8,10 +8,9 @@ class AmusementPark(Frame):
   
   def __init__(self, root):
 
-    global listbox
-    global AllTagList
-    global seeds
-    global seedBoxes
+    global seeds, seedBoxes
+
+    global lam, a, b, mu, sigma, alpha, theta
     
     Frame.__init__(self, root)
 
@@ -26,6 +25,44 @@ class AmusementPark(Frame):
       seedBoxes.append(newBox)
     Label(self, text = "Type in boxes to use your own values").grid(row = 10, column = 1, columnspan = 2)
 
+    #parameter controls
+    Label(self, text = "Parameters").grid(row = 1, column = 3, columnspan = 8)
+
+    Label(self, text = "Possion/Exponential", width = 20).grid(row = 2, column = 3, columnspan = 2)
+    Label(self, text = "Lambda").grid(row = 3, column = 3)
+    lam = Entry(self, width = 5)
+    lam.grid(row = 3, column = 4)
+    lam.insert(0, "4") 
+
+    Label(self, text = "Cont. Uniform").grid(row = 2, column = 5, columnspan = 2)
+    Label(self, text = "a").grid(row = 3, column = 5)
+    a = Entry(self, width = 5)
+    a.grid(row = 3, column = 6)
+    a.insert(0, "30") 
+    Label(self, text = "b").grid(row = 4, column = 5)
+    b = Entry(self, width = 5)
+    b.grid(row = 4, column = 6)
+    b.insert(0, "90") 
+
+    Label(self, text = "Normal", width = 10).grid(row = 2, column = 7, columnspan = 2)
+    Label(self, text = "Mu").grid(row = 3, column = 7)
+    mu = Entry(self, width = 5)
+    mu.grid(row = 3, column = 8)
+    mu.insert(0, "1") 
+    Label(self, text = "Sigma").grid(row = 4, column = 7)
+    sigma = Entry(self, width = 5)
+    sigma.grid(row = 4, column = 8)
+    sigma.insert(0, "0.3")
+
+    Label(self, text = "Gamma", width = 15).grid(row = 2, column = 9, columnspan = 2)
+    Label(self, text = "Alpha").grid(row = 3, column = 9)
+    alpha = Entry(self, width = 5)
+    alpha.grid(row = 3, column = 10)
+    alpha.insert(0, "tbd") 
+    Label(self, text = "Theta").grid(row = 4, column = 9)
+    theta = Entry(self, width = 5)
+    theta.grid(row = 4, column = 10)
+    theta.insert(0, "1")
   def genRand(self):
     seeds = []
     for i in range(8):
@@ -37,8 +74,8 @@ class AmusementPark(Frame):
 
 if __name__=='__main__':
   root = Tk()
-  root.resizable(width=True, height=True)
-  root.geometry('{}x{}'.format(500, 395))
+  root.resizable(width=False, height=False)
+  root.geometry('{}x{}'.format(1000, 800))
   AmusementPark.folderpath = ''
   AmusementPark.newfolderpath = ''
   AmusementPark.AllTagList = []
