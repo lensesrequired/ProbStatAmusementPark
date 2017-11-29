cf58dd65e476841fa193629a32982473e5138584
mylesbradley <mjbradley@coe.edu>
2017-11-28 18:56:46 -0600

    commented out the code so we can tell what the heck I was thinking.

--- step3.py
+++ step3.py
@@ -4,106 +4,82 @@ def main():
     ylist = [random.random() for i in range(8)]                              #8 random numbers each between 0 and 1
     x = [-(1/3)*math.log(1-y) for y in ylist]                                #Exponential distribution on how long it takes to get to the coaster
     arrivalTimes = []
-    famSize1 = [2, 3, 3, 1, 4, 2, 3, 4]                                            #test family sizes; this copy will be for the families we are waiting to arrive at the line for the coaster
-    famSize3 = famSize1[:]
-    allPeople = sum(famSize1)
-    lastFamSize = famSize1[-1]
+    famSize1 = [2, 3, 3, 1, 4, 2, 3, 4]                                      #test family sizes; this copy will be for the families we are waiting to arrive at the line for the coaster
+    famSize3 = famSize1[:]                                                   #this copy is for printing purposes only
+    allPeople = sum(famSize1)                                                #remember the total number of people coaster is expecting
+    lastFamSize = famSize1[-1]                                               #remember how many people are in the last family coaster is expecting
 
 
-    for i in range(len(famSize1)):                                          #this code loads arrival times assuming that there was one ticket booth, so this will need to be changed because we work with 2 ticket booths
+    for i in range(len(famSize1)):                                           #this code loads arrival times assuming that there was one ticket booth, so this will need to be changed because we work with 2 ticket booths
         for t in range(famSize1[i]):
-            arrivalTimes.append(sum(x[:i+1]))                                #sum arrival times to include the one we are one to append the total time since the first family arrived
+            arrivalTimes.append(sum(x[:i+1]))                                #add the arrival times for each family member from each family
 
-    famSize1 = [1]*allPeople
-    famSize2 = [1]*allPeople                                                   #this is for removing 4 people at a time from the line
-    #print(len(famSize1))
-    totalPeople = allPeople
+    famSize1 = [1]*allPeople                                                 #this is for the number of people we are expecting to come to the coaster that have not arrived yet
+    famSize2 = [1]*allPeople                                                 #this is for removing 4 people at a time from the line
+    totalPeople = allPeople                                                  #get copy of total number of people for remembering how many people have rode the coaster
 
-    waitTime = [0] * allPeople                                                       #give them each their own wait times
-    serviceTime = [0] * allPeople                                                    #give them each their own service times
+    waitTime = [0] * allPeople                                               #give each person their own wait times
+    serviceTime = [0] * allPeople                                            #give each person their own service times
     time = arrivalTimes[0]                                                   #start time going at arrival of first family
-    numQ = famSize1[0]                                                       #remember the number of people in the line
-    famSize1[0] = 0                                                          #have the copy of families we are waiting to arrive fam
-    doneTime = []                                                            #have a list to record done times
-    index = 0                                                                #start of index in list families that will/are being served 
+    numQ = 1                                                                 #start with at least one person in the line
+    famSize1[0] = 0                                                          #mark the first individual as having arrived
+    doneTime = []                                                            #have a list to record done times of each individual
+    index = 0                                                                #start of index in list of individuals that will/are being served 
     loadTime = 0                                                             #how long it takes to load and begin the next ride
-    endIndex = 1                                                             #end index in list of families that will/are being served
+    endIndex = 1                                                             #end index in list of individuals that will/are being served
 
 
     while(totalPeople > 0):
-        if(numQ <= 4 and len(famSize2) <= lastFamSize  and totalPeople <= 4):            #if we are on the last family
-           #print(len(doneTime))
+        if(numQ <= 4 and len(famSize2) <= lastFamSize  and totalPeople <= 4): #if we are on the last family
            totalPeople = 0                                                    #load them up and finish
-           #print(numQ)
-           #print(len(doneTime))
            numQ = 0
            y = random.random()
-           loadTime = (26*y+1)**(1/3)
+           loadTime = (26*y+1)**(1/3)                                         #calculate time to load on individuals
            for i in range(index, endIndex):
-               serviceTime[i] += 2 + loadTime
-               doneTime.append(time + 2 + loadTime)
+               serviceTime[i] += 2 + loadTime                                 #add the service time for the last individuals
+               doneTime.append(time + 2 + loadTime)                           #add the final done times
         
         else:
             if(numQ < 4 and len(famSize2) > 0):                               #if there is not enough people to ride the coaster
-                time += arrivalTimes[endIndex] - arrivalTimes[endIndex-1]     #wait for the next family
+                time += arrivalTimes[endIndex] - arrivalTimes[endIndex-1]     #wait for the next individual
                 for i in range(index, endIndex):
                     waitTime[i] += arrivalTimes[endIndex]-arrivalTimes[endIndex-1]
                 endIndex += 1
-                numQ += 1                           #add them to the line
-                famSize1[nonZero(famSize1)] = 0                               #set the first family number equal to 0
+                numQ += 1                                                     #add them to the line
+                famSize1[nonZero(famSize1)] = 0                               #mark that individual as having arrived
                 
             elif(numQ >= 4 or len(famSize2) == 0):
                 y = random.random()
                 loadTime = (26*y+1)**(1/3)                                    #calculate time it takes to unload and load ride and start it
-                for i in range(len(arrivalTimes)):                            #add every family that enters line load and ride time
-                    #print(str(i) + " " + str(time <= arrivalTimes[i] and arrivalTimes[i] <= time + 2 + loadTime and famSize1[i] > 0))
-                    #print(i, time, arrivalTimes[i], time+2+loadTime)
+                for i in range(len(arrivalTimes)):                            #add every individual that enters line during the ride
                     if(time <= arrivalTimes[i] and arrivalTimes[i] <= time + 2 + loadTime and famSize1[i] > 0):
-                        #print(str(i) + " just arrived")
                         numQ+=1
-                        #print(str(i) + " just arrived")
-                        #print(numQ)
-                        waitTime[i] += (time + 2 + loadTime) - arrivalTimes[i]
-                        famSize1[i] = 0
-                    elif i >= endIndex and famSize1[i] == 0:
-                        waitTime[i] += 2 + loadTime
-                #print(numQ)   
+                        waitTime[i] += (time + 2 + loadTime) - arrivalTimes[i] #calculate their respective wait times while the coaster is going
+                        famSize1[i] = 0                                        #mark each individual that arrives as having arrived
+                    elif i >= endIndex and famSize1[i] == 0:                  #if the person has arrived and their not riding the coaster
+                        waitTime[i] += 2 + loadTime                           #add the time to load the coaster and ride it as wait time for that person
                 time += 2 + loadTime                                          #add load and ride time to the total time
                 
-                for i in range(index, endIndex):
-                    serviceTime[i] += 2 + loadTime
-                #print(2+loadTime)
-                #print(serviceTime)
-                #print(numQ)
+                for i in range(index, endIndex):                              #for each person that is being served
+                    serviceTime[i] += 2 + loadTime                            #add the time to load and ride the coaster to their service times
                 numQ -= 4                                                     #subtract 4 people from the line
                 totalPeople -= 4                                              #subtract 4 people from the total people we know need to ride
                 sub = 4
-                #print("here")
                 while(sub > 0 and len(famSize2) > 0):
-                    if(famSize2[0] <= sub):                                   #if the remaining family can fit on the coaster with room to spare or fill it up
-                        sub -= 1                                    #load them onto the coaster
+                    if(famSize2[0] <= sub): 
+                        sub -= 1                                              #load them onto the coaster
                         famSize2.pop(0)                                       #remove them from our line copy
                         doneTime.append(time)                                 #record their time it took them to ride the coaster
-                 #       print(doneTime)
-                 #       print(len(doneTime))
-                    else:                                                     #if the remaining family can't all get on together
-                        famSize2[0] = famSize2[0] - sub                       #put on those that can fit on the coaster
-                        sub = 0
-                index = allPeople - len(famSize2)                                     #get index of next family that will be served
+                index = allPeople - len(famSize2)                             #get index of next individual that will be served
                 endIndex =index + first4(famSize2)+1                          #begin from start index and find how many families will be served in next ride
-                #print(len(famSize2))
-                #print(famSize2)
-                #print(numQ)
-                #print(numQ)
-                if(endIndex > allPeople):
+                if(endIndex > allPeople):                                     #cap endIndex at the total number of people in the park
                     endIndex = allPeople
+
+
+    #the rest of this (besides the helper functions) is just for checking the math that's been done
     for i in range(allPeople):
-        #print(len(doneTime))
-        #print(len(arrivalTimes))
-        #print(len(serviceTime))
-        #print(len(waitTime))
         if(doneTime[i] != arrivalTimes[i] + serviceTime[i] + waitTime[i]):
-            print(doneTime[i] - (arrivalTimes[i] + serviceTime[i] + waitTime[i]))
+            print(doneTime[i] - (arrivalTimes[i] + serviceTime[i] + waitTime[i])) #done time should equal how long it took an individual to arrive, wait for the coaster, and load and ride the coaster
     outfile = open("notes.txt", "w")
     print("family sizes", file = outfile)
     print(famSize3, file = outfile)
