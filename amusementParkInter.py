7a2b3500ae937061238b59941d2f2a6f225f74da
Anna <asmarek@coe.edu>
2017-11-29 10:08:08 -0600

    More stuff

--- amusementParkInter.py
+++ amusementParkInter.py
@@ -20,98 +20,68 @@ class AmusementPark(Frame):
 
     Frame.__init__(self, root)
 
-
-    #random number controls
-    Label(self, text = "Random Seeds").grid(row = 1, column = 1)
-    GenerateRand = Button(self, text='Generate', command=self.genRand)
-    GenerateRand.grid(row=1, column = 2)
-    self.seedBoxes = []
-    for i in range(8):
-      newBox = Entry(self)
-      newBox.grid(row = 2 + i, column = 1, columnspan = 2)
-      self.seedBoxes.append(newBox)
-    Label(self, text = "Type in boxes to use your own values").grid(row = 10, column = 1, columnspan = 2)
-
+    Label(self, text = "At any point in the program you can type in your own random numbers").grid(row = 0, column = 1, columnspan = 8)
+    Label(self, text = "and hit a recalculate button to use your own values").grid(row = 1, column = 1, columnspan = 8)
     #parameter controls
     self.setParameterControls()
 
     #a couple useful buttons...
     Arrivals = Button(self, text='Arrival of Cars!', command=self.next)
     Arrivals.grid(row = 6, column = 4, columnspan = 2)
-    
-  def genRand(self):
-    for i in range(8):
-      newRand = random.random()
-      self.seedBoxes[i].delete(0, END)
-      self.seedBoxes[i].insert(0, str(newRand)) 
 
   def setParameterControls(self):
-    Label(self, text = "Parameters").grid(row = 1, column = 3, columnspan = 8)
+    Label(self, text = "Set Parameters").grid(row = 2, column = 1, columnspan = 8)
 
-    Label(self, text = "Possion/Exponential", width = 20).grid(row = 2, column = 3, columnspan = 2)
-    Label(self, text = "Lambda").grid(row = 3, column = 3)
+    Label(self, text = "Possion/Exponential", width = 20).grid(row = 3, column = 1, columnspan = 2)
+    Label(self, text = "Lambda").grid(row = 4, column = 1)
     self.lam = Entry(self, width = 5)
-    self.lam.grid(row = 3, column = 4)
+    self.lam.grid(row = 4, column = 2)
     self.lam.insert(0, "4")
 
-    Label(self, text = "Cont. Uniform").grid(row = 2, column = 5, columnspan = 2)
-    Label(self, text = "a").grid(row = 3, column = 5)
+    Label(self, text = "Cont. Uniform").grid(row = 3, column = 3, columnspan = 2)
+    Label(self, text = "a").grid(row = 4, column = 3)
     self.a = Entry(self, width = 5)
-    self.a.grid(row = 3, column = 6)
+    self.a.grid(row = 4, column = 4)
     self.a.insert(0, "30") 
-    Label(self, text = "b").grid(row = 4, column = 5)
+    Label(self, text = "b").grid(row = 5, column = 3)
     self.b = Entry(self, width = 5)
-    self.b.grid(row = 4, column = 6)
+    self.b.grid(row = 5, column = 4)
     self.b.insert(0, "90") 
 
-    Label(self, text = "Normal", width = 10).grid(row = 2, column = 7, columnspan = 2)
-    Label(self, text = "Mu").grid(row = 3, column = 7)
+    Label(self, text = "Normal", width = 10).grid(row = 3, column = 5, columnspan = 2)
+    Label(self, text = "Mu").grid(row = 4, column = 5)
     self.mu = Entry(self, width = 5)
-    self.mu.grid(row = 3, column = 8)
+    self.mu.grid(row = 4, column = 6)
     self.mu.insert(0, "1") 
-    Label(self, text = "Sigma").grid(row = 4, column = 7)
+    Label(self, text = "Sigma").grid(row = 5, column = 5)
     self.sigma = Entry(self, width = 5)
-    self.sigma.grid(row = 4, column = 8)
+    self.sigma.grid(row = 5, column = 6)
     self.sigma.insert(0, "0.3")
 
-    Label(self, text = "Gamma", width = 15).grid(row = 2, column = 9, columnspan = 2)
-    Label(self, text = "Alpha").grid(row = 3, column = 9)
-    self.alpha = Entry(self, width = 5)
-    self.alpha.grid(row = 3, column = 10)
-    self.alpha.insert(0, "tbd") 
-    Label(self, text = "Theta").grid(row = 4, column = 9)
+    Label(self, text = "Gamma", width = 15).grid(row = 3, column = 7, columnspan = 2)
+    Label(self, text = "Theta").grid(row = 4, column = 7)
     self.theta = Entry(self, width = 5)
-    self.theta.grid(row = 4, column = 10)
+    self.theta.grid(row = 4, column = 8)
     self.theta.insert(0, "1")
 
   def next(self):
     AOCroot = Tk()
     AOCroot.resizable(width=False, height=False)
-    AOCroot.geometry('{}x{}'.format(775, 275))    
-    seeds = []
-    for i in self.seedBoxes:
-      seeds.append(float(i.get()))
+    AOCroot.geometry('{}x{}'.format(950, 275))    
     params = [0 for i in range(7)]
     params[LAM] = self.lam.get()
     params[A] = self.a.get()
     params[B] = self.b.get()
     params[MU] = self.mu.get()
     params[SIGMA] = self.sigma.get()
-    params[ALPHA] = self.alpha.get()
     params[THETA] = self.theta.get()
-    ArrivalOfCars(AOCroot, seeds, params).grid()
+    ArrivalOfCars(AOCroot, params).grid()
 
   def animate(self):
     pass
 
 class ArrivalOfCars(Frame):
-  global seeds
-  global params
-  global serviceTimes, serviceBoxes
-  global doneBoxes, queueBoxes
-
-  def __init__(self, root, s, p):
-    self.seeds = s
+  def __init__(self, root, p):
     self.params = p
 
     Frame.__init__(self, root)
@@ -122,35 +92,49 @@ class ArrivalOfCars(Frame):
       Label(self, text = str(i + 1)).grid(row = 2 + i, column = 0)
 
     #Arrival Time calculations
+    #random number controls
+    Label(self, text = "Random Seeds").grid(row = 1, column = 1)
+    self.seedBoxes = []
+    self.seeds = []
+    for i in range(8):
+      newBox = Entry(self, width = 15)
+      newBox.grid(row = 2 + i, column = 1)
+      self.seedBoxes.append(newBox)
+      self.seeds.append(random.random())
+      self.seedBoxes[i].insert(0, str(self.seeds[i])) 
+
     dist = [-(1/float(self.params[LAM]))*math.log(1-seed) for seed in self.seeds]
     self.arrivalTimes = [sum(dist[:i+1]) for i in range(8)]
-    Label(self, text = "Arrival Times (minutes)").grid(row = 1, column = 1)
+    Label(self, text = "Arrival Times (minutes)").grid(row = 1, column = 2)
+    self.arrivalTimeBoxes = []
     for i, a in enumerate(self.arrivalTimes):
-      Label(self, text = str(a)).grid(row = 2 + i, column = 1)
+      newLabel = Label(self, text = str(a))
+      newLabel.grid(row = 2 + i, column = 2)
+      self.arrivalTimeBoxes.append(newLabel)
 
     #Service Time calculations
     randomNums = [random.random() for i in range(8)]
     self.randomNumBoxes = []
-    Label(self, text = "Random Nums").grid(row = 1, column = 2)
+    Label(self, text = "Random Nums").grid(row = 1, column = 3)
     for i, r in enumerate(randomNums):
-      newEntry = Entry(self)
+      newEntry = Entry(self, width = 15)
       newEntry.insert(0, str(r))
-      newEntry.grid(row = 2 + i, column = 2)
+      newEntry.grid(row = 2 + i, column = 3)
       self.randomNumBoxes.append(newEntry)
 
     self.serviceBoxes = []
     self.serviceTimes = [float(self.params[A]) + (randomNums[i]*(float(self.params[B])-float(self.params[A]))) for i in range(8)]
-    Label(self, text = "Service Times (seconds)").grid(row = 1, column = 3)
+    Label(self, text = "Service Times (seconds)").grid(row = 1, column = 4)
     for i, s in enumerate(self.serviceTimes):
       newEntry = Label(self, text = str(s))
-      newEntry.grid(row = 2 + i, column = 3)
+      newEntry.grid(row = 2 + i, column = 4)
       self.serviceBoxes.append(newEntry)
 
     #Queue and Done time calculations
     self.queueBoxes = []
     queue1 = []
     queue2 = []
-    Label(self, text = "Queue", width = 8).grid(row = 1, column = 4)
+    Label(self, text = "Queue", width = 8).grid(row = 1, column = 5)
 
     self.doneTimes = []
     for i in range(0, 8):
@@ -173,7 +157,7 @@ class ArrivalOfCars(Frame):
           queue1.append(self.arrivalTimes[i] +(float(self.serviceTimes[i])/60))          
           self.doneTimes.append(self.arrivalTimes[i] + (float(self.serviceTimes[i])/60))
         l = Label(self, text = "Left")
-        l.grid(row = 2 + i, column = 4)
+        l.grid(row = 2 + i, column = 5)
         self.queueBoxes.append(l)
       else:
         if(len(queue2) > 0):
@@ -183,31 +167,46 @@ class ArrivalOfCars(Frame):
           queue2.append(self.arrivalTimes[i] +(float(self.serviceTimes[i])/60))          
           self.doneTimes.append(self.arrivalTimes[i] + (float(self.serviceTimes[i])/60))
         l = Label(self, text = "Right")
-        l.grid(row = 2 + i, column = 4)
+        l.grid(row = 2 + i, column = 5)
         self.queueBoxes.append(l)
 
     self.doneBoxes = []
-    Label(self, text = "Done Times (minutes)").grid(row = 1, column = 5)
+    Label(self, text = "Done Times (minutes)").grid(row = 1, column = 6)
     for i, d in enumerate(self.doneTimes):
       newL = Label(self, text = str(d))
-      newL.grid(row = 2 + i, column = 5)
+      newL.grid(row = 2 + i, column = 6)
       self.doneBoxes.append(newL)
 
 
     #a couple useful buttons...
-    Recalc = Button(self, text='Recalculate service times!', command=self.calculate)
-    Recalc.grid(row = 10, column = 2, columnspan = 1)
+    RecalcArrival = Button(self, text='Recalculate Arrival times!', command=self.calcArrival)
+    RecalcArrival.grid(row = 10, column = 2, columnspan = 1)
+    RecalcService = Button(self, text='Recalculate service times!', command=self.calcService)
+    RecalcService.grid(row = 10, column = 4, columnspan = 1)
     Entrance = Button(self, text='Move to Park Entrance!', command=self.next)
     Entrance.grid(row = 10, column = 3, columnspan = 1)
 
-  def calculate(self):
+  def calcArrival(self):
+    self.seeds = []
+    for i in range(8):
+      self.seeds.append(float(self.seedBoxes[i].get()))
+
+    dist = [-(1/float(self.params[LAM]))*math.log(1-seed) for seed in self.seeds]
+    self.arrivalTimes = [sum(dist[:i+1]) for i in range(8)]
+
+    for i, a in enumerate(self.arrivalTimes):
+      self.arrivalTimeBoxes[i].config(text = str(a))
+
+    self.calcService()
+
+  def calcService(self):
     randomNums = []
     for i, r in enumerate(self.randomNumBoxes):
       randomNums.append(float(r.get()))
 
     self.serviceTimes = [float(self.params[A]) + (randomNums[i]*(float(self.params[B])-float(self.params[A]))) for i in range(8)]
     for i, s in enumerate(self.serviceTimes):
-      serviceBoxes[i].config(text = str(s))
+      self.serviceBoxes[i].config(text = str(s))
 
     queue1 = []  #append done times to each queue
     queue2 = []
@@ -247,7 +246,7 @@ class ArrivalOfCars(Frame):
 
   def next(self):
     PEroot = Tk()
-    PEroot.resizable(width=False, height=False)
+    PEroot.resizable(width=True, height=False)
     PEroot.geometry('{}x{}'.format(720, 550))
     ParkEntrance(PEroot, self.arrivalTimes, self.serviceTimes, self.doneTimes, self.params).grid()
 
@@ -274,7 +273,7 @@ class ParkEntrance(Frame):
     self.YBoxes = []
     Label(self, text = "Random Nums").grid (row = 1, column = 1)
     for i, s in enumerate(self.Ys):
-      newEntry = Entry(self)
+      newEntry = Entry(self, width = 15)
       newEntry.insert(0, str(s))
       newEntry.grid(row = 2 + i, column = 1)
       self.YBoxes.append(newEntry)
@@ -286,7 +285,7 @@ class ParkEntrance(Frame):
 
     self.Xs = [(float(self.params[SIGMA])*z) + float(self.params[MU]) if (float(self.params[SIGMA])*z) + float(self.params[MU]) > 0 else 0 for z in self.Zs]
     self.XBoxes = []
-    Label(self, text = "Movement Times (minutes)").grid(row = 1, column = 2)
+    Label(self, text = "Movement Times (min)").grid(row = 1, column = 2)
     for i, s in enumerate(self.Xs):
       newEntry = Label(self, text = str(s))
       newEntry.grid(row = 2 + i, column = 2)
@@ -295,7 +294,7 @@ class ParkEntrance(Frame):
     #Display enter queue time
     self.queueEnterTimes = []
     self.queueEnterBoxes = []
-    Label(self, text = "Enter Queue Time (minutes)").grid (row = 1, column = 3)
+    Label(self, text = "Enter Queue Time (min)").grid (row = 1, column = 3)
     for i, x in enumerate(self.Xs):
       newEntry = Label(self, text = str(x + self.startTimes[i][0]))
       newEntry.grid(row = 2 + i, column = 3)
@@ -312,9 +311,9 @@ class ParkEntrance(Frame):
     #Calculate and display family sizes
     self.randomNums = [random.random() for i in range(8)]
     self.randomNumBoxes = []
-    Label(self, text = "Random Nums").grid (row = 12, column = 1)
+    Label(self, text = "Random Nums").grid(row = 12, column = 1)
     for i, s in enumerate(self.randomNums):
-      newEntry = Entry(self)
+      newEntry = Entry(self, width = 15)
       newEntry.insert(0, str(s))
       newEntry.grid(row = 13 + i, column = 1)
       self.randomNumBoxes.append(newEntry)
@@ -339,15 +338,20 @@ class ParkEntrance(Frame):
       self.familySizeBoxes.append(newEntry)
 
     #Ticketbooth nonsense
-
+    self.gamRandomBoxes = []
+    Label(self, text = "Random Nums").grid(row = 12, column = 3, columnspan = 4)
+    for i in range(32):
+      newEntry = Entry(self, width = 15)
+      newEntry.insert(0, str(random.random()))
+      newEntry.grid(row = 13 + (i//4), column = 3 + (i%4))
+      self.gamRandomBoxes.append(newEntry)
 
     #Helpful Buttons
-    RecalcMovement = Button(self, text='Recalculate Movement Times!', command=self.calcMove)
+    RecalcMovement = Button(self, text='Recalculate Move Times!', command=self.calcMove)
     RecalcMovement.grid(row = 10, column = 2, columnspan = 1)
     RecalcFamilies = Button(self, text='Recalculate Family Sizes!', command=self.calcSize)
     RecalcFamilies.grid(row = 21, column = 2, columnspan = 1)
-    
-
+  
   def calcMove(self):
     self.Ys = []
     for i, y in enumerate(self.YBoxes):
@@ -391,6 +395,6 @@ class ParkEntrance(Frame):
 if __name__=='__main__':
   root = Tk()
   root.resizable(width=False, height=False)
-  root.geometry('{}x{}'.format(720, 275))
+  root.geometry('{}x{}'.format(490, 250))
   AmusementPark(root).grid()
   root.mainloop()
